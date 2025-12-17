632dee88fc505d59833d2f26ec0a8cc1f6074d1c
Daniel Wyrwał <daniel.wyrwal@gmail.com>
2025-12-17 12:38:40 +0100

    Dodano iostream

--- main.cpp
+++ main.cpp
@@ -1,4 +1,13 @@
-pierwsza linijka master druga linijka branch1 trzecia linijka master2 trzecia
-    linijka branch czwarta linijka master piąta linijka branch2 szósta linijka
-        master siódma linijka master ósma linijka master dziewiąta linijka
-            master dziesiąta linijka master
+#include <iostream>
+
+pierwsza linijka master
+druga linijka branch1
+trzecia linijka master2
+trzecia linijka branch
+czwarta linijka master
+piąta linijka branch2
+szósta linijka master
+siódma linijka master
+ósma linijka master
+dziewiąta linijka master
+dziesiąta linijka master
